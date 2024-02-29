4a009197ef3cc1317e0740cb51a6f5430c2da2d9
Felix Breuer <fbreuer@pm.me>
2024-02-29 21:08:36 +0100

    fix: rendering
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,6 @@
-import { hydrate, prerender as ssr } from 'preact-iso';
-
 import './base.css';
 import './todo.css';
+import { render } from 'preact';
 import App from './app'
 
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
-}
-
-export async function prerender(data) {
-	return await ssr(<App {...data} />);
-}
+render(<App />, document.getElementById('app'));
